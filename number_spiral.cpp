ba5fdd55b65b248425f7cb1e4fff9fb0d6609a3f
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2025-08-18 20:32:38 +0530

    Add files via upload

--- number_spiral.cpp
+++ number_spiral.cpp
@@ -9,54 +9,62 @@
 using namespace std;
 #define int long long
  
-void solve([[maybe_unused]] int test) {
-   int x,y;
-   cin>>x>>y;
-   if(x==y){
-        if(x==1){
-            cout<<1<<'\n';
-            return;
-        }
-        else{
-            int a = x*x;
-            cout<<a-x+1<<'\n';
-            return;
-        }
-   }
-   else if(x<y){
-        if(y%2==0){
-            int n=(y-1)*(y-1) +1;
-            int a = n+x-1;
-            cout<<a<<'\n';
-            return;
-        }
-        else{
-            int n=y*y;
-            int a=n-x+1;
-            cout<<a<<'\n';
-            return;
-        }
-   }
-   else{
-        if(x%2==0){
-            int n=x*x;
-            int a=n-y+1;
-            cout<<a<<'\n';
-            return;
-        }
-        else{
-            int n=(x-1)*(x-1)+1;
-            int a=n+y-1;
-            cout<<a<<'\n';
-            return;
-        }
-   }
-}
+// void solve([[maybe_unused]] int test) {
+//    int x,y;
+//    cin>>x>>y;
+//    if(x==y){
+//         if(x==1){
+//             cout<<1<<'\n';
+//             return;
+//         }
+//         else{
+//             int a = x*x;
+//             cout<<a-x+1<<'\n';
+//             return;
+//         }
+//    }
+//    else if(x<y){
+//         if(y%2==0){
+//             int n=(y-1)*(y-1) +1;
+//             int a = n+x-1;
+//             cout<<a<<'\n';
+//             return;
+//         }
+//         else{
+//             int n=y*y;
+//             int a=n-x+1;
+//             cout<<a<<'\n';
+//             return;
+//         }
+//    }
+//    else{
+//         if(x%2==0){
+//             int n=x*x;
+//             int a=n-y+1;
+//             cout<<a<<'\n';
+//             return;
+//         }
+//         else{
+//             int n=(x-1)*(x-1)+1;
+//             int a=n+y-1;
+//             cout<<a<<'\n';
+//             return;
+//         }
+//    }
+// }
  
 int32_t main() {
     int t = 1;
-    cin >> t;
+    //cin >> t;
     for (int tt = 1; tt <= t; tt++) {
-        solve(tt);
+        int n;
+        cin>>n;
+        int ans = 0;
+        int c = 5;
+        while(c<=n){
+            ans+=n/c;
+            c=c*5;
+        }
+        cout<<ans<<'\n';
     }
 }
