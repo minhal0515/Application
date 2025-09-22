57a5c48eae47552ccd7cfb5782213ca8850e729d
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2025-09-22 14:07:40 +0530

    Add files via upload

--- mj.cpp
+++ mj.cpp
@@ -1,5 +1,5 @@
 #include "bits/stdc++.h"
- 
+
 #ifdef local
 #include "custom/debugger.h"
 #else
@@ -10,44 +10,75 @@ using namespace std;
 #define int long long
  
 void solve([[maybe_unused]] int test) {
-   int n,x;
-   cin>>n>>x;
-   string s;
-   cin>>s;
-  int inf=1e9;
-   int c=0;
-   int c1 = -inf;
-   int c2=inf;
-   for(int i=0;i<n;i++){
-        if(s[i]=='#'){
-            c++;
+    int n,m;
+    cin>>n>>m;
+    cin.ignore();
+    vector<int> ans(m);
+    vector<vector<int>> v(n);
+    for(int i=0;i<n;i++){
+        int m1;
+        cin>>m1;
+        v[i].resize(m1);
+        for(int j=0;j<m1;j++){
+            cin>>v[i][j];
         }
-   }
-
-   //cout<<c1<<c2;
-   
-   if(c==0||(x==1||x==n)){
-    cout<<1<<'\n';
-    return;
-   }
-   x--;
-      for(int i=x-1;i>=0;i--){
-    if(s[i]=='#'){
-        c1=i;
-        break;
-        }
-   }
-   for(int i=x+1;i<n;i++){
-    if(s[i]=='#'){
-        c2=i;
-        break;
     }
-   }
+    if(n==2){
+        int f=0;
+        for(int i=0;i<n;i++){
+            for(int u:v[i]){
+                ans[u-1]++;
+                //cout<<ans[u]<<' ';
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]<2){
+                f=1;
+                break;
+            }
+        }
+        if(f==1){
+            cout<<"NO"<<'\n';
+        } else{
+            cout<<"YES"<<'\n';
+        }
+    }
+    else{
+        int f=0;
+        for(int i=0;i<n;i++){
+            for(int u:v[i]){
+                ans[u-1]++;
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]==0){
+                cout<<"NO"<<'\n';
+                return;
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            int ff=0;
+            for (int p:v[i]){
+                if(ans[p-1]==1 && ff==0){
+                    ff=1;
+                    //cout<<p<<' ';
+                    c++;
+                }
+            }
+        }
+//        cout<<n<<' '<<c<<' ';
+        if(n-c<2){
 
-   int m1 = min(x+1,n-c2+1);
-   int m2 = min(n-x, c1+2);
-   int ans = max(m1,m2);
-   cout<<ans<<'\n';
+            f=1;
+        }
+        if(f==0){
+            cout<<"YES"<<'\n';
+        } else{
+            cout<<"NO"<<'\n';
+        }
+    }
+    
 }
  
 int32_t main() {
