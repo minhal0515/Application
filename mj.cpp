c63a84d5b323fed36c41cc1b2bb5d2221d1bd1bc
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2025-10-03 22:52:10 +0530

    Add files via upload

--- mj.cpp
+++ mj.cpp
@@ -10,74 +10,31 @@ using namespace std;
 #define int long long
  
 void solve([[maybe_unused]] int test) {
-    int n,m;
-    cin>>n>>m;
-    cin.ignore();
-    vector<int> ans(m);
-    vector<vector<int>> v(n);
-    for(int i=0;i<n;i++){
-        int m1;
-        cin>>m1;
-        v[i].resize(m1);
-        for(int j=0;j<m1;j++){
-            cin>>v[i][j];
-        }
-    }
-    if(n==2){
-        int f=0;
-        for(int i=0;i<n;i++){
-            for(int u:v[i]){
-                ans[u-1]++;
-                //cout<<ans[u]<<' ';
-            }
-        }
-        for(int i=0;i<ans.size();i++){
-            if(ans[i]<2){
-                f=1;
-                break;
-            }
-        }
-        if(f==1){
-            cout<<"NO"<<'\n';
+    int n,ak,bk,ad,bd;
+    cin>>n>>ak>>bk>>ad>>bd;
+    int ans = 0;
+    if(ak>ad && bk>bd){
+        ans = max(n-ad, n-bd);
+    } else if(bk<bd && ak>ad){
+        ans = max(bd, n-ad);
+    } else if(ak<ad && bk>bd){
+        ans=max(n-bd, ad);
+    } else if(ak<ad && bk<bd){
+        ans = max(ad,bd);
+    } else if(ak==ad){
+        if(bk>bd){
+            ans = n-bd;
         } else{
-            cout<<"YES"<<'\n';
+            ans=bd;
         }
-    }
-    else{
-        int f=0;
-        for(int i=0;i<n;i++){
-            for(int u:v[i]){
-                ans[u-1]++;
-            }
-        }
-        for(int i=0;i<ans.size();i++){
-            if(ans[i]==0){
-                cout<<"NO"<<'\n';
-                return;
-            }
-        }
-        int c=0;
-        for(int i=0;i<n;i++){
-            int ff=0;
-            for (int p:v[i]){
-                if(ans[p-1]==1 && ff==0){
-                    ff=1;
-                    //cout<<p<<' ';
-                    c++;
-                }
-            }
-        }
-//        cout<<n<<' '<<c<<' ';
-        if(n-c<2){
-
-            f=1;
-        }
-        if(f==0){
-            cout<<"YES"<<'\n';
+    } else if(bk==bd){
+        if(ak>ad){
+            ans=n-ad;
         } else{
-            cout<<"NO"<<'\n';
+            ans=ad;
         }
     }
+    cout<<ans<<'\n';
     
 }
  
