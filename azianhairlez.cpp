f5336035263aec4bf018803aa4ef6e9c3c807504
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2025-10-13 14:23:41 +0530

    Add files via upload

--- azianhairlez.cpp
+++ azianhairlez.cpp
@@ -4,59 +4,26 @@
 using namespace std;
 #define int long long
 int32_t main(){
-	int t;
-	cin>>t;
-	for(int i=0;i<t;i++){
-		int n;
-		cin>>n;
-		vector <int> v(2*n);
-		for(int i=0;i<2*n;i++){
+	int n;
+	cin>>n;
+	vector<int> v(n);
+	//for(int i=0;i<n;i++){
+		for(int i=0;i<n;i++){
 			cin>>v[i];
 		}
-		if(n==1){
-			cout<<v[1]-v[0]<<'\n';
-			continue;
-		}
-		vector <int> ans(n);
-		int even_sum = 0;
-		//int odd_sum = 0;
-		int even_p = 1;
-		int odd_p = 2;
-		for(int i=0;i<2*n;i++){
-			if(i%2==0){
-				even_sum-=v[i];
-			} else{
-				even_sum+=v[i];
-			}
-		}
-		ans[0]=even_sum;
-		int odd_sum = v[2*n-1]-v[0];
-		for(int i=1;i<2*n-1;i++){
-			if(i%2!=0){
-				odd_sum-=v[i];
-			} else{
-				odd_sum+=v[i];
-			}	
-		}
-		ans[1]=odd_sum;
-//		cout<<2*v[2*n-even_p]<<"HAHA";
-		for(int i=2;i<n;i++){
-			if(i%2==0){
-				even_sum-=2*v[even_p];
-				even_sum+=2*v[2*n-even_p-1];
-				even_p+=2;
-				ans[i]=even_sum;
-			} else{
-				odd_sum-=2*v[odd_p];
-				odd_sum+=2*v[2*n-odd_p-1];
-				odd_p+=2;
-				ans[i]=odd_sum;
-			}
-		}
+		vector <int> v1(n);
 		for(int i=0;i<n;i++){
-			cout<<ans[i]<<' ';
+			v1[v[i]-1]=i+1;
 		}
-		cout<<'\n';
-
-	}
+		// for(int i=0;i<n;i++){
+		// 	cout<<v1[i];
+		// }
+		int ans=1;
+		for(int i=0;i<n-1;i++){
+			if(v1[i]>v1[i+1]){
+				ans++;
+			}
+		}
+		cout<<ans<<'\n';
+	
 }
