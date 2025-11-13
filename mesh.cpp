2f1acd38dc5e20a6a03ad59e2f0e4e8cae37c5c8
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2025-11-13 13:55:57 +0530

    Add files via upload

--- mesh.cpp
+++ mesh.cpp
@@ -2,42 +2,30 @@
 using namespace std;
 #define int long long
 
+int expo(int b, int e, int m){
+	int res=1;
+	b%=m;
+	while(e>0){
+		if(e&1){
+			res=(res*b)%m;
+
+		}
+		b=(b*b)%m;
+		e>>=1;
+	}	
+	return res;
+}
+
 int32_t main(){
 	int t;
 	cin>>t;
+	int n=1000000007;
 	for(int i=0;i<t;i++){
-		int n,k;
-		cin>>n>>k;
-		vector <int> v(n);
-		vector<int> mp(n+1);
-		vector <int> p(n+1);
-		for(int i=0;i<n;i++){
-			cin>>v[i];
-			mp[v[i]]++;
-		}
-		for(int i=1;i<=n;i++){
-			p[i]=p[i-1]+mp[i];
-		}
-		int ans=1;
-		for(int g=1;g<=n;g++){
-			int t=min(n,4*g-1);
-			int gg=n-p[t];
-			if(g<=n){
-				gg+=mp[g];
-			}
-			if(2*g<=n){
-				gg+=mp[2*g];
-			}
-			if(3*g<=n){
-				gg+=mp[3*g];
-			}
-			if(gg>=n-k){
-				ans=g;
-			}
-		}
+		int a,b;
+		cin>>a>>b;
+		int ans = expo(a,b,n);
+		//ans=ans%n;
 		cout<<ans<<'\n';
+		
 	}
-
-
-
 }
