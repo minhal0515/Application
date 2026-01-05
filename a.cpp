a40fc75268cb2238329396cf1854127ba80e7485
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-01-05 19:58:45 +0530

    Add files via upload

--- a.cpp
+++ a.cpp
@@ -21,15 +21,52 @@ int32_t main() {
         string s;
         cin>>s;
         int c=0;
+        int f=1;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='Y'){
+            if(s[i]=='u'){
                 c++;
             }
         }
-        if(c>=2){
-            cout<<"NO"<<'\n';
+        if(c==0){
+            //f=0;
+            cout<<0<<'\n';
+            continue;
+        }
+        if(s[0]=='s' && s[s.size()-1]=='s'){
+            for(int i=1;i<s.size()-1;i++){
+                if(s[i]!=s[i-1]&&s[i]!=s[i+1]){
+                    continue;
+                } else{
+                    f=0;
+                }
+            }
         } else{
-            cout<<"YES"<<'\n';
+            f=0;
+        }
+        int count=0;
+        if(f==1){
+            cout<<0<<'\n';
+        } else{
+            if(s[0]=='u'){
+                count++;
+                s[0]='s';
+            }
+            if(s[s.size()-1]=='u'){
+                count++;
+                s[s.size()-1]='s';
+            }
+            for(int i=1;i<s.size()-1;i++){
+                if(s[i]=='u'){
+                    // if(s[i-1]!='s'){
+                    //     count++;
+                    // }
+                    if(s[i+1]!='s'){
+                        count++;
+                        s[i+1]='s';
+                    }
+                }
+            }
+            cout<<count<<'\n';
         }
     }
 }
