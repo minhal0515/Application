143cd1c8c37ea0c6b1f4b2c8fd21af02de5bbb93
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-06-03 10:24:19 +0530

    Add files via upload

--- thisisit.cpp
+++ thisisit.cpp
@@ -14,35 +14,43 @@ int32_t main() {
     int t = 1;
     cin >> t;
     for (int tt = 1; tt <= t; tt++) {
-        int n;
-        cin>>n;
-        vector <int> v(n);
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-        }
-        sort(v.begin(),v.end());
-        int m=0;
-        int x=0;
-        for(int i=0;i<n;i++){
-            if(v[i]==m){
-                m++;
-                x=i;
-            } 
-        }
-        int sum;
-        if(m==0){
-            sum=v[n-1];
-        }else{
-            sum=1;
-        }
-        int c=2;
-        while(c<=m){
-            sum+=(c+c-1);
-            c++;
-        }
-        int u=((n-1)*(v[n-1]+m))+v[n-1];
-        sum+=(n-x-1)*(v[n-1]+m);
-        sum=max(sum,u);
-        cout<<sum<<'\n';
+        int n,x,ss;
+        cin>>n>>x>>ss;
+        string s;
+        cin>>s;
+        int l=0;
+        int r=0;
+        int ans=0;
+        for(char c:s){
+            if(c=='A'){
+                if(x*ss==ans){
+                    continue;
+                }
+                ans++;
+                if(ans>l*ss){
+                    l++;
+                }
+                r=min(x,r+1);
+            }
+            else if(c=='I'){
+                if(l==x){
+                    continue;
+                }
+                ans++;
+                l++;
+                r=min(x,r+1);
+            }
+            else if(c=='E'){
+                if(ans==r*ss){
+                    continue;
+                }
+                ans++;
+                if(ans>l*ss){
+                    l++;
+                }
+            }
+
+        }   
+        cout<<ans<<'\n';     
     }
 }
