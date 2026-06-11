52b4a541cad3d8765b1ed2547b42732562e7894c
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-06-11 23:33:03 +0530

    Add files via upload

--- philsalt.cpp
+++ philsalt.cpp
@@ -14,27 +14,47 @@ int32_t main() {
     int t = 1;
     cin >> t;
     for (int tt = 1; tt <= t; tt++) {
-        int n;
-        cin>>n;
-        int a,b;
-        int f=0;
-        for(int i=0;i<=22;i++){
-            if(i==10||i==12||i==13||i==14||i==15||i==16||i==17||i==18||i==19||i==20||i==21){
-                continue;
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        int va=INT_MAX;
+        string ans(n,'0'); 
+        for (int i=0;i<=n;i++){
+            string t=s;
+            string a1(n,'0');
+            int k1 = k;
+ 
+            for (int j=0; j<i; j++){
+                if (t[j]=='(' && k1 > 0){
+                    a1[j] = '1';
+                    t[j] = ')';
+                    k1--;
+                }
             }
-            if(((n%12)==(i%12))&&((n-i)%12==0)&&(n-i)>=0){
-                
-                    a=i;
-                    b=n-a;
-                    f=1;
-                    break;
-                    
+            for (int j=n-1; j>i; j--){
+                if (t[j] == ')' && k1 > 0){
+                    a1[j]='1';
+                    t[j] = '(';
+                    k1--;
+                }
             }
-        }
-        if(f==0){
-            cout<<-1<<'\n';
-            continue;
-        }
-        cout<<a<<' '<<b<<'\n';
+            int v = 0;
+            int b = 0;
+            for (int j=0; j<n; j++){
+                if (b>0 && t[j]==')'){
+                    v += 2;
+                    b--;
+                }
+                else if (t[j]=='('){
+                    b++;
+                }
+            }
+            if (va>v){
+                va=v;
+                ans=a1;
+            }
+        } 
+        cout<<ans<<'\n';
     }
 }
