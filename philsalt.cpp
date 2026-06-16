cd28a7845f2dd9061d6f501383723661e743756e
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-06-16 14:57:31 +0530

    Add files via upload

--- philsalt.cpp
+++ philsalt.cpp
@@ -12,49 +12,23 @@ using namespace std;
  
 int32_t main() {
     int t = 1;
-    cin >> t;
+    //cin >> t;
     for (int tt = 1; tt <= t; tt++) {
-        int n,k;
-        cin>>n>>k;
-        string s;
-        cin>>s;
-        int va=INT_MAX;
-        string ans(n,'0'); 
-        for (int i=0;i<=n;i++){
-            string t=s;
-            string a1(n,'0');
-            int k1 = k;
- 
-            for (int j=0; j<i; j++){
-                if (t[j]=='(' && k1 > 0){
-                    a1[j] = '1';
-                    t[j] = ')';
-                    k1--;
-                }
+        int n;
+        cin>>n;
+        vector <int> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        vector <int> v2;
+        for(int i=0;i<n;i++){
+            auto it = upper_bound(v2.begin(), v2.end(), v[i]);
+            if(it==v2.end()){
+                v2.push_back(v[i]);
+            } else{
+                v2[it-v2.begin()]=v[i];
             }
-            for (int j=n-1; j>i; j--){
-                if (t[j] == ')' && k1 > 0){
-                    a1[j]='1';
-                    t[j] = '(';
-                    k1--;
-                }
-            }
-            int v = 0;
-            int b = 0;
-            for (int j=0; j<n; j++){
-                if (b>0 && t[j]==')'){
-                    v += 2;
-                    b--;
-                }
-                else if (t[j]=='('){
-                    b++;
-                }
-            }
-            if (va>v){
-                va=v;
-                ans=a1;
-            }
-        } 
-        cout<<ans<<'\n';
+        }
+        cout<<v2.size()<<'\n';
     }
 }
