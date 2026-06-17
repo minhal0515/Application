f869dc6455ab937946f51cfd75164cdab99162fc
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-06-17 10:20:27 +0530

    Add files via upload

--- philsalt.cpp
+++ philsalt.cpp
@@ -11,24 +11,28 @@ using namespace std;
 
  
 int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     int t = 1;
     //cin >> t;
     for (int tt = 1; tt <= t; tt++) {
-        int n;
-        cin>>n;
-        vector <int> v(n);
+        int x,n;
+        cin>>x>>n;
+        set<int> s1{0,x};
+        multiset<int> s2{x};
         for(int i=0;i<n;i++){
-            cin>>v[i];
+            int p;
+            cin>>p;
+            auto it1 = s1.upper_bound(p);
+            auto it2 = it1;
+            it2--;
+            s2.erase(s2.find(*it1-*it2));
+            s2.insert(*it1-p);
+            s2.insert(p-*it2);
+            s1.insert(p);
+            auto a=s2.end();
+            a--;
+            cout<<*a<<' ';
         }
-        vector <int> v2;
-        for(int i=0;i<n;i++){
-            auto it = upper_bound(v2.begin(), v2.end(), v[i]);
-            if(it==v2.end()){
-                v2.push_back(v[i]);
-            } else{
-                v2[it-v2.begin()]=v[i];
-            }
-        }
-        cout<<v2.size()<<'\n';
     }
 }
