2987604db6c6470047a40aa5eef93cd23c8c69bd
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-06-24 19:02:35 +0530

    Add files via upload

--- dodge.cpp
+++ dodge.cpp
@@ -8,32 +8,30 @@
  
 using namespace std;
 #define int long long
+const int m=1e9+7;
+const int tmi=500000004;
+
+int ts(int s, int e){
+    return((((e-s+1)%m)*((s+e)%m)%m)*tmi%m);
+}
  
 void solve([[maybe_unused]] int test) {
     int n;
     cin>>n;
-    vector <int> v(n);
-    for(int i=0;i<n;i++){
-        cin>>v[i];
+    int t=0;
+    int at=1;
+    while(at<=n){
+        int a1=n/at;
+        int ls=n/a1;
+        t=(t+a1*ts(at,ls))%m;
+        at=ls+1;
     }
-    int m=0;
-    for(int i=0;i<n-1;i++){
-        if(v[i]>m){
-            m=v[i];
-        }
-        if(v[i+1]<v[i]){
-            v[i+1]=v[i]+v[i+1];
-        }
-    }
-    if(v[n-1]>m){
-        m=v[n-1];
-    }
-    cout<<m<<'\n';
+    cout<<t<<'\n';
 }
  
 int32_t main() {
     int t = 1;
-    cin >> t;
+   // cin >> t;
     for (int tt = 1; tt <= t; tt++) {
         solve(tt);
     }
