dcb90dce345df1cfc8a6435fa05eaa0c3f817f14
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-07-05 15:44:42 +0530

    Add files via upload

--- dodge.cpp
+++ dodge.cpp
@@ -8,41 +8,37 @@
  
 using namespace std;
 #define int long long
-const int m=1e9+7;
+const int m=200005;
 const int tmi=500000004;
 
- 
-void solve([[maybe_unused]] int test) {
-    int n;
-    cin>>n;
-    int s=0;
-    int ans=0;
-    vector <int> a(n+1);
-    for(int i=1;i<=n;i++){
-        cin>>a[i];
-        s^=a[i];
-    }
-    if(n==1){
-        cout<<0<<'\n';
-        return;
-    }
-    if(!s){
-        cout<<1<<'\n';
-        return;
-    }
-    for(int i=1;i<=n;i++){
-        if((s^a[i])<a[i]){
-            ans++;
+int n;
+vector<vector<int>> adj(m);
+bool d[m];
+int ans=0; 
+void dfs(int p, int c){
+    for(int i:adj[c]){
+        if(i!=p){
+            dfs(c,i);
+            if(!d[i] && !d[c]){
+                d[c]=d[i]=1;
+                ans++;
+            }
         }
     }
-    ans%=998244353;
-    cout<<ans<<'\n';
 }
  
 int32_t main() {
     int t = 1;
-    cin >> t;
+    //cin >> t;
     for (int tt = 1; tt <= t; tt++) {
-        solve(tt);
+        cin>>n;
+        for(int i=0;i<n-1;i++){
+            int a,b;
+            cin>>a>>b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }  
+        dfs(0,1);
+        cout<<ans<<'\n';
     }
 }
