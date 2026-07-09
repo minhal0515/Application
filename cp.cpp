145c61be4caf1111b3b5e033d9f9d64ff06c8950
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-07-09 21:44:00 +0530

    Add files via upload

--- cp.cpp
+++ cp.cpp
@@ -10,23 +10,35 @@ using namespace std;
 #define int long long
  
 void solve([[maybe_unused]] int test) {
-    int n,x,y;
-    cin>>n>>x>>y;
-    vector <int> a(n);
+    string a,b;
+    cin>>a>>b;
+    int n=a.size();
+    int m=b.size();
+    vector<int> pa(n+1);
+    vector<int> pb(m+1);
     for(int i=0;i<n;i++){
-        cin>>a[i];
+        pa[i+1]=(pa[i]+(a[i]-'0'))%10;
     }
-    int sum=0;
-    for(int i=0;i<n;i++){
-        sum+=(a[i]/x)*y;
+    for(int i=0;i<m;i++){
+        pb[i+1]=(pb[i]+(b[i]-'0'))%10;
+    }
+
+    if(pa.back()!=pb.back()){
+        cout<<-1<<'\n';
+        return;
     }
     int ans=0;
-    for(int i=0;i<n;i++){
-        if(a[i]+sum-((a[i]/x)*y)>ans){
-            ans=a[i]+sum-((a[i]/x)*y);
+    vector<vector<int>>dp(n+1,vector<int>(m+1));
+    for(int j=1;j<=m;j++){
+        for(int i=1;i<=n;i++){
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            if(pa[i]==pb[j]){
+                dp[i][j]=dp[i-1][j-1]+1;
+            }
         }
     }
-    cout<<ans<<'\n';
+    cout<<dp[n][m]<<'\n';
+
 }
  
 int32_t main() {
