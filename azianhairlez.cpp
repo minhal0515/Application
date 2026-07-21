8342f803d42dcfb2830eba6fc8d234c4d9e2452b
minhal0515 <152763964+minhal0515@users.noreply.github.com>
2026-07-21 17:01:03 +0530

    Add files via upload

--- azianhairlez.cpp
+++ azianhairlez.cpp
@@ -3,34 +3,48 @@
 #include <cstdint>
 using namespace std;
 #define int long long
+
+
+int xorf(const vector <int>& v){
+	int val=v[0];
+	for(int i=1;i<v.size();i++){
+		val^=v[i];
+	}
+	return val;
+}
+
 int32_t main(){
 	int t;
 	cin>>t;
 	for(int i=0;i<t;i++){
 		int n;
 		cin>>n;
-		vector<int> v(n+1);
-		for(int i=1;i<=n;i++){
-			cin>>v[i];
+		vector <int> a(n);
+		vector <int> b(n);
+		for(int i=0;i<n;i++){
+			cin>>a[i];
 		}
-		int c=1;
-		vector <int> ans(n+1, 0);
-		ans[1]=c;
-		for(int i=2;i<=n;i++){
-			int d=v[i]-v[i-1];
-			int p=i-d;
-			if(p==0){
-				c++;
-				ans[i]=c;
-			} else{
-				ans[i]=ans[p];
+		for(int i=0;i<n;i++){
+			cin>>b[i];
+		}
+		int a1=xorf(a);
+		int b1=xorf(b);
+		if(a1==b1){
+			cout<<"Tie"<<'\n';
+			continue;
+		}
+		string adv;
+		for(int i=0;i<n;i++){
+			if(a[i]!=b[i]){
+				if(i%2==0){
+					adv="Ajisai";
+				} else{
+					adv="Mai";
+				}
 			}
 		}
-//		cout<<c;
-		for(int i=1;i<=n;i++){
-			cout<<ans[i]<<' ';
-		}
-		cout<<'\n';
+		cout<<adv<<'\n';
+		
 	}
 	
 }
